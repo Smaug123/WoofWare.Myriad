349f29e0ccbc727e67b7a62da537b18920414b93
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-12-27 22:03:46 +0000

    Fix analyser

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       dotnet-sdk = pkgs.dotnet-sdk_8;
       dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
       version = "0.1";
-      dotnetTool = toolName: toolVersion: sha256:
+      dotnetTool = dllOverride: toolName: toolVersion: sha256:
         pkgs.stdenvNoCC.mkDerivation rec {
           name = toolName;
           version = toolVersion;
@@ -29,18 +29,23 @@
             sha256 = sha256;
             installPhase = ''mkdir -p $out/bin && cp -r tools/net6.0/any/* $out/bin'';
           };
-          installPhase = ''
+          installPhase = let
+            dll =
+              if dllOverride == null
+              then name
+              else dllOverride;
+          in ''
             runHook preInstall
             mkdir -p "$out/lib"
             cp -r ./bin/* "$out/lib"
-            makeWrapper "${dotnet-runtime}/bin/dotnet" "$out/bin/${name}" --add-flags "$out/lib/${name}.dll"
+            makeWrapper "${dotnet-runtime}/bin/dotnet" "$out/bin/${name}" --add-flags "$out/lib/${dllOverride}.dll"
             runHook postInstall
           '';
         };
     in {
       packages = {
-        fantomas = dotnetTool "fantomas" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fantomas.version "sha256-Jmo7s8JMdQ8SxvNvPnryfE7n24mIgKi5cbgNwcQw3yU=";
-        fsharp-analyzers = dotnetTool "fsharp-analyzers" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fsharp-analyzers.version "sha256-yGc4oV1fEAfeWUf/y60VUeps3lJWnvVjomWFeVEyXVE=";
+        fantomas = dotnetTool null "fantomas" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fantomas.version "sha256-Jmo7s8JMdQ8SxvNvPnryfE7n24mIgKi5cbgNwcQw3yU=";
+        fsharp-analyzers = dotnetTool "FSharp.Analyzers.Cli" "fsharp-analyzers" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fsharp-analyzers.version "sha256-yGc4oV1fEAfeWUf/y60VUeps3lJWnvVjomWFeVEyXVE=";
         fetchDeps = let
           flags = [];
           runtimeIds = ["win-x64"] ++ map (system: pkgs.dotnetCorePackages.systemToDotnetRid system) dotnet-sdk.meta.platforms;
