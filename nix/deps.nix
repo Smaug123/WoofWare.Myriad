581f5647b49d16136598c3d3d1aa446292779c1c
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-01-29 19:08:14 +0000

    Reformat

--- nix/deps.nix
+++ nix/deps.nix
@@ -8,7 +8,7 @@
   })
   (fetchNuGet {
     pname = "fantomas";
-    version = "6.3.0-alpha-005";
+    version = "6.3.0-alpha-007";
     sha256 = "sha256-uZw6h6k/DS4BcYtK9cv8TLS0H8MZDO3WBaPPTdtTgu0=";
   })
   (fetchNuGet {
