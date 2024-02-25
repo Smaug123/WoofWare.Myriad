2a0cd4b837388e0ea035f6151e4dc73c5b882b85
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-02-25 09:57:41 +0000

    No yaml pls

---
+++ .github/workflows/tag.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export TAG
+TAG=$(find . -maxdepth 1 -type f -name 'WoofWare.Myriad.Plugins.*.nupkg' -exec sh -c 'basename "$1" .nupkg' shell {} \;)
+
+curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Smaug123/WoofWare.Myriad/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}'
