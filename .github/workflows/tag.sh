3656729a479000d4c92ab0e776c5aa5df11309b6
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-02-25 11:18:24 +0000

    Fix

--- .github/workflows/tag.sh
+++ .github/workflows/tag.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 export TAG
-TAG=$(find . -maxdepth 1 -type f -name 'WoofWare.Myriad.Plugins.*.nupkg' -exec sh -c 'basename "$1" .nupkg' shell {} \;)
+TAG=$(find . -maxdepth 1 -type f -name 'WoofWare.Myriad.Plugins.*.nupkg' -exec sh -c 'basename "$1" .nupkg' shell {} \; | grep -v Attributes)
+
+case "$TAG" in
+  *"
+"*)
+    echo "Error: TAG contains a newline; multiple plugins found."
+    exit 1
+    ;;
+esac
 
 # target_commitish empty indicates the repo default branch
 echo curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Smaug123/WoofWare.Myriad/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}'
