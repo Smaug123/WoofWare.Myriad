ee61b43aa8398a7941a3b8bfbc9f2fbc4bb7c9bd
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-02-25 11:42:05 +0000

    Fix

--- .github/workflows/tag.sh
+++ .github/workflows/tag.sh
@@ -12,4 +12,4 @@ case "$TAG" in
 esac
 
 # target_commitish empty indicates the repo default branch
-curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Smaug123/WoofWare.Myriad/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}'
+curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Smaug123/WoofWare.Myriad/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}'
