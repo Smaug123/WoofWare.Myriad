c696dcf31f48d085e562cffeacd5124c11563b83
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2024-05-30 12:35:30 +0100

    Fix curl failing logic (#147)

--- .github/workflows/tag.sh
+++ .github/workflows/tag.sh
@@ -14,7 +14,7 @@ case "$TAG" in
 esac
 
 # target_commitish empty indicates the repo default branch
-if curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Smaug123/WoofWare.Myriad/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}' > curl_output.json; then
+if curl --fail-with-body -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Smaug123/WoofWare.Myriad/releases -d '{"tag_name":"'"$TAG"'","target_commitish":"","name":"'"$TAG"'","draft":false,"prerelease":false,"generate_release_notes":false}' > curl_output.json; then
     echo "Curl succeeded."
 else
     exit_message=$(jq -r --exit-status 'if .errors | length == 1 then .errors[0].code else null' curl_output.json)
