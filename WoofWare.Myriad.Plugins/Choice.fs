63f187673dbefdec0670739ee22806b50f454b22
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-05-30 10:37:38 +0100

    Delete unnecessary file

--- WoofWare.Myriad.Plugins/Choice.fs
+++
@@ -1,8 +0,0 @@
-namespace WoofWare.Myriad.Plugins
-
-[<RequireQualifiedAccess>]
-module internal Choice =
-    let get<'a> (choice : Choice<'a, 'a>) : 'a =
-        match choice with
-        | Choice1Of2 a -> a
-        | Choice2Of2 a -> a
