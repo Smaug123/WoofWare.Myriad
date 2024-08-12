79152ec38998397e0daf53b4b262bedc0db8528d
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-08-12 12:30:37 +0100

    Bump deps

--- nix/deps.nix
+++ nix/deps.nix
@@ -28,8 +28,8 @@
   })
   (fetchNuGet {
     pname = "fsharp-analyzers";
-    version = "0.26.0";
-    hash = "sha256-60Bl36LOb/zVNdH2SBSuQ5O41lP9dKTNZbs5vvYs+3U=";
+    version = "0.26.1";
+    hash = "sha256-UqSK8PoXRJMZWKphMmWpl5klVn82EM1VczNZ1AOZ6Sw=";
   })
   (fetchNuGet {
     pname = "FSharp.Core";
@@ -173,8 +173,8 @@
   })
   (fetchNuGet {
     pname = "Nerdbank.GitVersioning";
-    version = "3.6.139";
-    hash = "sha256-DMEdNlYh9tqkqQ/98zwk7NcRYBpTApLiFwzkgaHP7Fo=";
+    version = "3.6.141";
+    hash = "sha256-i1pBJ12vlPmde6qSQK4PG2QLSpjaUCoY+odTi24R5XI=";
   })
   (fetchNuGet {
     pname = "NETStandard.Library";
