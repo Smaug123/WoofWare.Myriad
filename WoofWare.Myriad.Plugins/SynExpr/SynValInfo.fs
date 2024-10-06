d478afbc4e0d16e6c896cd3747c23984ec2e5c57
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-10-06 21:13:55 +0100

    Use WoofWare.Whippet.Fantomas, not Myriad.Core

--- WoofWare.Myriad.Plugins/SynExpr/SynValInfo.fs
+++
@@ -1,7 +0,0 @@
-namespace WoofWare.Myriad.Plugins
-
-open Fantomas.FCS.Syntax
-
-[<RequireQualifiedAccess>]
-module internal SynValInfo =
-    let empty = SynValInfo.SynValInfo ([], SynArgInfo.empty)
